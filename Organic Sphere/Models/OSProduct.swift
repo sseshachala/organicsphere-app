3fe6c983e16691e4a7ace7aebdc2d79aba3cb2ec
Harshdeep Singh <harsh.batth@gmail.com>
2016-10-29 13:02:56 -0400

    Adding service layer and replacing the acitivity indicator view

---
+++ Organic Sphere/Models/OSProduct.swift
@@ -0,0 +1,13 @@
+//
+//  OSProduct.swift
+//  Organic Sphere
+//
+//  Created by Harshdeep Singh on 2016-10-29.
+//  Copyright © 2016 Arshdeep Singh. All rights reserved.
+//
+
+import UIKit
+
+class OSProduct: NSObject {
+
+}
---
+++ Organic Sphere/Services/OSCategoryService.swift
@@ -0,0 +1,13 @@
+//
+//  OSCategoryService.swift
+//  Organic Sphere
+//
+//  Created by Harshdeep Singh on 2016-10-29.
+//  Copyright © 2016 Arshdeep Singh. All rights reserved.
+//
+
+import UIKit
+
+class OSCategoryService: NSObject {
+
+}
